aeb5d443a177c54c40f089bef0ab7050dd377aaf
Travis Yeik <tyeik@huskers.unl.edu>
2023-06-06 09:07:22 -0600

    initial

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppImageButton;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MauiAppImageButton;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        SemanticScreenReader.Announce(CounterBtn.Text);
+        if (imgBtn.BorderColor == Colors.Red)
+        {
+            imgBtn.BorderColor = Colors.Green;
+        }
+        else
+        {
+            imgBtn.BorderColor = Colors.Red;
+        }
+
+        CounterBtn.Text = $"Button border color is {imgBtn.BorderColor}";
+    }
+}
+
